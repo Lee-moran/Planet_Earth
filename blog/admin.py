30e67926d3ef3fc53ad7a65dd3545a5626160ff1
Lee-moran <leemoran06@hotmail.com>
2022-07-12 10:58:07 +0000

    view creation, import code institute stock html

--- blog/admin.py
+++ blog/admin.py
@@ -4,14 +4,21 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-     """
+    """
     Use Summernote for Blog admin
     """
     list_display = ('title', 'slug', 'status', 'created_on')
     search_fields = ['title', 'content']
     list_filter = ('status', 'created_on')
-    prepopulated_fields = {'slug': ('title')}
+    prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content')
+    actions = ['approve_posts']
+
+    def approve_posts(self, request, queryset):
+        """
+        approval of comments
+        """
+        queryset.update(approved=True)
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
