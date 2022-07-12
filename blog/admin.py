4509d1f7204892922bf70168ad53ab0f9e82afe8
Lee-moran <leemoran06@hotmail.com>
2022-07-12 15:53:46 +0000

    blogs avaialable to view and read

--- blog/admin.py
+++ blog/admin.py
@@ -30,6 +30,7 @@ class CommentAdmin(admin.ModelAdmin):
     search_fields = ['name', 'email', 'body']
     actions = ['approve_comments']
 
+
     def approve_comments(self, request, queryset):
         """
         approval of comments
--- blog/views.py
+++ blog/views.py
@@ -12,24 +12,24 @@ MVT - model,view,teplates
 '''
 
 
-class PostList (generic.ListView):
+class PostList(generic.ListView):
     model = Post
-    queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'index.html'
+    queryset = Post.objects.filter(status=1).order_by("-created_on")
+    template_name = "index.html"
     paginate_by = 6
 
 
-class PostDetail(view):
+class PostDetail(View):
 
     def get(self, request, slug, *args, **kwargs):
-        queryset = Post.object.filter(status=1)
+        queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.filter(approved=True).order_by('created_on')
+        comments = post.comments.filter(approved=True).order_by("-created_on")
         liked = False
-        if post.like.filter(id=self.request.user.id).exisits():
+        if post.likes.filter(id=self.request.user.id).exists():
             liked = True
 
-            return render(
+        return render(
             request,
             "post_detail.html",
             {
