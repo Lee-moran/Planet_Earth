a2774c5413bdae0289f39dd46b4fb982e9d39c47
Lee-moran <leemoran06@hotmail.com>
2022-07-15 14:10:59 +0000

    commenting section working

--- blog/views.py
+++ blog/views.py
@@ -41,11 +41,9 @@ class PostDetail(View):
                 "comment_form": CommentForm()
             },
         )
-
-
-class PostDetail(View):
-
+    
     def post(self, request, slug, *args, **kwargs):
+
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by("-created_on")
@@ -54,10 +52,9 @@ class PostDetail(View):
             liked = True
 
         comment_form = CommentForm(data=request.POST)
-
         if comment_form.is_valid():
             comment_form.instance.email = request.user.email
-            comment_form.instance.name = request.user.usename
+            comment_form.instance.name = request.user.username
             comment = comment_form.save(commit=False)
             comment.post = post
             comment.save()
@@ -71,7 +68,7 @@ class PostDetail(View):
                 "post": post,
                 "comments": comments,
                 "commented": True,
-                "liked": liked,
-                "comment_form": CommentForm()
+                "comment_form": comment_form,
+                "liked": liked
             },
         )
