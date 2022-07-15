28ef0912af87bf941c226e7c3360201210fdee8d
Lee-moran <leemoran06@hotmail.com>
2022-07-15 15:23:46 +0000

    btn added for likes and applied authentication to btn

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,15 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
+from django.http import HttpResponseRedirect
 from .models import Post
 from .forms import CommentForm
 
 # Create your views here.
 
-'''
-class based views - allow us to make
-resuable views which inherit from each other
-allows us yo view our blog
-MVT - model,view,teplates
-'''
-
+# class based views - allow us to make 
+# resuable views which inherit from each other
+# allows us yo view our blog 
+# MVT - model,view,teplates
 
 class PostList(generic.ListView):
     model = Post
@@ -72,3 +70,15 @@ class PostDetail(View):
                 "liked": liked
             },
         )
+
+
+class PostLike(View):
+    
+    def post(self, request, slug, *args, **kwargs):
+        post = get_object_or_404(Post, slug=slug)
+        if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
+        else:
+            post.likes.add(request.user)
+
+        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
