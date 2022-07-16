eb02079b9d810077a87a2167928f05297852d889
Lee-moran <leemoran06@hotmail.com>
2022-07-16 11:50:04 +0000

    added flash messages

--- planet/settings.py
+++ planet/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from django.contrib.messages import constants as messages
 import dj_database_url
 if os.path.isfile("env.py"):
     import env
@@ -59,6 +60,14 @@ INSTALLED_APPS = [
 ]
 
 SITE_ID = 1
+
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-info',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+    }
 
 # login and out reurning to home page 
 
