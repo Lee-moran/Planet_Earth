d9ee59aafb806f7290f94810a3e7c202e57716ea
Lee-moran <leemoran06@hotmail.com>
2022-07-16 14:15:38 +0000

    your blogs.html template create and url path applied

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.views import generic, View
 from django.http import HttpResponseRedirect
 from .models import Post
 from .forms import CommentForm
+from django.core.paginator import Paginator
 
 # Create your views here.
 
@@ -82,4 +83,5 @@ class PostLike(View):
             post.likes.add(request.user)
 
         return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+
 
